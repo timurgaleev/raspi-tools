0633bb1dba33c64ab004221389a1a78c2c13fb1b
Timur Galeev <timur_galeev@outlook.com>
2021-06-20 21:32:09 +0200

    Changed nginx

--- rpi-monitoring/nginx/main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-envsubst '${PROXY_UPSTREAM_INFLUXDB_HOST}${PROXY_UPSTREAM_INFLUXDB_PORT}${PROXY_UPSTREAM_GRAFANA_HOST}${PROXY_UPSTREAM_GRAFANA_PORT}' < /etc/nginx/nginx.conf.tpl > /etc/nginx/nginx.conf
-nginx -g "daemon off;"
